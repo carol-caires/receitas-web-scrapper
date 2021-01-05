812d2ee5dc90b29851cef46dc6b83af86165e757
Carol Caires <carolbazarello@gmail.com>
2021-01-04 21:54:05 -0300

    add port env variable

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get("/search/:name", (req, res, next) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => {
+    console.log("Server running on port "+port);
 });
